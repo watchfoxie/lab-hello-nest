df57fc541327fc998ddd8a391bb1a7e56867578d
watchfoxie <tocana.mar@gmail.com>
2025-09-11 10:43:17 +0300

    nestjs - init hello app

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API Description')
+    .setVersion('1.0')
+    .addTag('your-tag') // Optional: Add tags for categorization
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(process.env.PORT ?? 3000);
+}
+void bootstrap();
